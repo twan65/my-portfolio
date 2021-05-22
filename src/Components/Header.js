1e4bf743dadd1d445ad6c011dd19c682d4ce7449
twan65 <cx.twan65@gmail.com>
2021-05-23 03:50:29 +0900

    JsonData修正

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const project = this.props.data.project;
+    const blog = this.props.data.blog;
     const github = this.props.data.github;
     const name = this.props.data.name;
     const description = this.props.data.description;
@@ -62,13 +62,13 @@ class Header extends Component {
               <h1 className="responsive-headline">{name}</h1>
             </Fade>
             <Fade bottom duration={1200}>
-              <h3>{description}.</h3>
+              <h3>{description}</h3>
             </Fade>
             <hr />
             <Fade bottom duration={2000}>
               <ul className="social">
-                <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
+                <a href={blog} className="button btn blog-btn">
+                  <i className="fa fa-book"></i>blog
                 </a>
                 <a href={github} className="button btn github-btn">
                   <i className="fa fa-github"></i>Github
