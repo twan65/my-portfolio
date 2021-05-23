d25f2e59d3022aff6e723b248fe6c21d206e36a2
twan65 <cx.twan65@gmail.com>
2021-05-23 23:03:36 +0900

    Fotter social icons削除

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,23 +5,11 @@ class Footer extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const networks = this.props.data.social.map(function (network) {
-      return (
-        <li key={network.name}>
-          <a href={network.url}>
-            <i className={network.className}></i>
-          </a>
-        </li>
-      );
-    });
-
     return (
       <footer>
         <div className="row">
           <Fade bottom>
             <div className="twelve columns">
-              <ul className="social-links">{networks}</ul>
-
               <ul className="copyright">
                 <li>ありがとうございます。</li>
               </ul>
