dbe65ccc87868831a4ffbac78bd26c6b623dc9d2
twan65 <cx.twan65@gmail.com>
2021-05-23 22:47:00 +0900

    CSS ID変更

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -11,7 +11,7 @@ class Portfolio extends Component {
       let projectImage = "images/portfolio/" + projects.image;
 
       return (
-        <div key={id++} className="columns portfolio-item">
+        <div key={id++} className="columns interest-skill-item">
           <div className="item-wrap">
             <Zmage alt={projects.title} src={projectImage} />
             <div style={{ textAlign: "center" }}>{projects.title}</div>
@@ -25,10 +25,10 @@ class Portfolio extends Component {
         <Fade left duration={1000} distance="40px">
           <div className="row">
             <div className="twelve columns collapsed">
-              <h1>最近、興味を持っている技術い。</h1>
+              <h1>最近、興味を持っている技術</h1>
 
               <div
-                id="portfolio-wrapper"
+                id="interest-skill-wrapper"
                 className="bgrid-quarters s-bgrid-thirds cf"
               >
                 {projects}
